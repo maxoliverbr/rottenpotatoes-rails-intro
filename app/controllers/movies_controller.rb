8280fda0765f951072a329c7feef38d1f88d5aa7
Platinum Anchovies <max.oliver@cintrax.com.br>
2022-09-09 04:05:54 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,15 +9,12 @@ class MoviesController < ApplicationController
   def index
     
     @all_ratings = Movie.all_ratings
-    
-    if params[:ratings].nil? and params[:sort].nil?
-      if not session[:sort].nil? and not session[:ratings_to_show].nil?
-        redirect_to movies_path(sort: session[:sort], ratings: session[:ratings_to_show])
-      else
-        redirect_to movies_path(sort: "title", ratings: {"G"=>"1", "PG"=>"1", "PG-13"=>"1", "NC-17"=>"1", "R"=>"1"})
-      end
+ 
+    if params[:sort].nil? && params[:ratings].nil? &&
+      (!session[:sort].nil? || !session[:ratings].nil?)
+      redirect_to movies_path(:sort => session[:sort], :ratings => session[:ratings])
     end
-    
+
     if not params[:ratings].nil?
       @ratings_to_show = params[:ratings]
       @selected_ratings = params[:ratings]
